9776124469f82a9ec075409eef71d4f28dc60b17
Thinker28 <84234983+Thinker28@users.noreply.github.com>
2022-02-01 21:37:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useRef, useState } from 'react';
 import './App.css';
 
 import firebase from 'firebase/compat/app';
-import ScrollToBottom from 'react-scroll-to-bottom';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 import 'firebase/compat/analytics';
